436b9865ad238250ce155238382978a01da7ee88
zhanghuan <2387749855@qq.com>
2021-04-19 08:40:12 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import PriceList from './components/PriceList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+  {
+    id: 1,
+    title: '去云南旅行',
+    price: 200,
+    date: '2018-09-10',
+    category: {
+      id: 1,
+      name: '旅行',
+      type: 'outcome'
+    }
+  },
+  {
+    id: 2,
+    title: '去云南旅行',
+    price: 400,
+    date: '2018-10-10',
+    category: {
+      id: 1,
+      name: '旅行',
+      type: 'outcome'
+    }
+  }
+]
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <PriceList 
+          items = {items}
+          onModifyItem={(item) => {alert(item.id)}}
+          onDeleteItem={(item) => {alert(item.id)}}
+        />
+      </div>
+    );
+  }
+
 }
 
 export default App;
