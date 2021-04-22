4f53a529494c2e87cf273f95d75329ddbd13af04
zhanghuan <2387749855@qq.com>
2021-04-22 08:56:58 +0800

    完成home页面交互

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,57 +3,130 @@ import logo from "../logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import PriceList from "../components/PriceList";
-import { LIST_VIEW, CHART_VIEW, TYPE_INCOME, TYPE_OUTCOME } from "../utility";
+import {
+    LIST_VIEW,
+    CHART_VIEW,
+    TYPE_INCOME,
+    TYPE_OUTCOME,
+    parseToYearAndMonth,
+    padLeft
+} from "../utility";
 import ViewTab from "../components/ViewTab";
 import MonthPicker from "../components/MonthPicker";
 import TotalPrice from "../components/TotalPrice";
-import CreateBtn from '../components/CreateBtn';
+import CreateBtn from "../components/CreateBtn";
+
+const categoies = {
+    1: {
+        id: "1",
+        name: "旅行",
+        type: "outcome",
+        iconName: "ios-plane",
+    },
+    2: {
+        id: "2",
+        name: "理财",
+        type: "income",
+        iconName: "logo-yen",
+    },
+};
 
 const items = [
     {
         id: 1,
         title: "去云南旅行",
         price: 200,
-        date: "2018-09-10",
-        category: {
-            id: 1,
-            name: "旅行",
-            type: "outcome",
-            iconName: "ios-plane",
-        },
+        date: "2021-04-10",
+        cid: 1,
     },
     {
         id: 2,
         title: "去云南旅行",
         price: 400,
-        date: "2018-10-10",
-        category: {
-            id: 1,
-            name: "旅行",
-            type: "outcome",
-            iconName: "ios-plane",
-        },
+        date: "2021-03-10",
+        cid: 1,
     },
     {
         id: 3,
         title: "理财收入",
         price: 200,
-        date: "2018-11-10",
-        category: {
-            id: 1,
-            name: "理财",
-            type: "income",
-            iconName: "logo-yen",
-        },
-
-    }
+        date: "2021-02-10",
+        cid: 2,
+    },
 ];
 
+const newItem = {
+    id: 4,
+    title: "新添加的项目",
+    price: 300,
+    date: "2021-04-20",
+    cid: 1,
+};
+
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items,
+            currentDate: parseToYearAndMonth(),
+            tabView: LIST_VIEW,
+        };
+    }
+
+    changeView = (view) => {
+        this.setState({
+            tabView: view,
+        });
+    };
+
+    changeDate = (year, month) => {
+        this.setState({
+            currentDate: {year, month}
+        })
+    };
+
+    modifyItem = (modifyItem) => {
+        const modifiedItems = this.state.items.map((item) => {
+            if (item.id === modifyItem.id) {
+                return {
+                    ...item,
+                    title: "更新后的标题",
+                };
+            } else {
+                return item;
+            }
+        });
+        this.setState({
+            items: modifiedItems
+        })
+    };
+
+    createItem = () => {
+        this.setState({
+            items: [newItem, ...this.state.items],
+        });
+    };
+
+    deleteItem = (deletedItem) => {
+        const filteredItems = this.state.items.filter(
+            (item) => item.id !== deletedItem.id
+        );
+        this.setState({
+            items: filteredItems,
+        });
+    };
+
     render() {
+        const { items, currentDate, tabView } = this.state;
+        const itemsWidthCategory = items.map((item) => {
+            item.category = categoies[item.cid];
+            return item;
+        }).filter(item => {
+            return item.date.includes(`${currentDate.year}-${padLeft(currentDate.month)}`)
+        });
         let totalIncome = 0,
             totalOutcome = 0;
-        items.forEach((item) => {
+        itemsWidthCategory.forEach((item) => {
             if (item.category.type === TYPE_OUTCOME) {
                 totalOutcome += item.price;
             } else {
@@ -69,9 +142,9 @@ class Home extends Component {
                     <div className="row">
                         <div className="col">
                             <MonthPicker
-                                year={2018}
-                                month={8}
-                                onChange={() => {}}
+                                year={currentDate.year}
+                                month={currentDate.month}
+                                onChange={this.changeDate}
                             />
                         </div>
                         <div className="col">
@@ -83,13 +156,20 @@ class Home extends Component {
                     </div>
                 </header>
                 <div className="content-area py-3 px-3">
-                    <ViewTab activeTab={LIST_VIEW} onTabChange={() => {}}/>
-                    <CreateBtn onClick={() => {}} />
-                    <PriceList 
-                        items={items}
-                        onModifyItem={() => {}}
-                        onDeleteItem={() => {}}
+                    <ViewTab
+                        activeTab={tabView}
+                        onTabChange={this.changeView}
                     />
+                    <CreateBtn onClick={this.createItem} />
+
+                    {tabView === LIST_VIEW && (
+                        <PriceList
+                            items={itemsWidthCategory}
+                            onModifyItem={this.modifyItem}
+                            onDeleteItem={this.deleteItem}
+                        />
+                    )}
+                    {tabView === CHART_VIEW && <h1>这里是图表区域</h1>}
                 </div>
             </Fragment>
         );
--- src/utility.js
+++ src/utility.js
@@ -14,3 +14,12 @@ export const range = (size, startAt = 0) => {
     }
     return arr;
 }
+
+export const parseToYearAndMonth = (str) => {
+    const date = str ? new Date(str) : new Date();
+
+    return {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1
+    }
+}
